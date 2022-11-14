6b54da020ab0f92b5d578e8d64881d33661f987d
senyka0 <Arsk293@gmail.com>
2022-11-14 14:22:45 +0000

    adding react swiper

--- src/components/Waves.tsx
+++ src/components/Waves.tsx
@@ -1,5 +1,9 @@
 import { Wave } from "../utils/waves";
 import React, { FC } from "react";
+import { Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 interface Props {
   waves: Wave[];
@@ -10,27 +14,26 @@ const Waves: FC<Props> = ({ waves, totalWaves }) => {
   return (
     <div className="mt-20 mx-auto max-w-xl sm:mt-0">
       <div className="text-xl font-bold text-gray-900 text-center sm:my-5 ">Total messages: {totalWaves}</div>
-      <div className="h-44 sm:h-40 md:h-72 overflow-auto w-auto scrollbar-thin">
+      <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
         {waves.map((wave, index) => {
           return (
-            <div key={index} className="mx-2 p-5 relative block border-black border-2 sm:p-8 mt-3">
-              <span className="absolute inset-x-0 bottom-0 h-2 bg-gradient-to-r from-green-300 via-blue-500 to-purple-600"></span>
-
-              <div className="justify-between sm:flex">
-                <div>
-                  <h3 className="text-lg sm:text-xl font-bold text-gray-900">{wave.message}</h3>
-
-                  <p className="text-xs mt-1 sm:text-xs sm:font-medium text-gray-600">From: {wave.address}</p>
+            <SwiperSlide key={index}>
+              <div className="mx-3 sm:mx-0 p-5 relative block border-black border-2 sm:p-8 mt-3">
+                <span className="absolute inset-x-0 bottom-0 h-2 bg-gradient-to-r from-green-300 via-blue-500 to-purple-600"></span>
+                <div className="justify-between sm:flex">
+                  <div>
+                    <h3 className="text-lg sm:text-xl font-bold text-gray-900">{wave.message}</h3>
+                    <p className="text-xs mt-1 sm:text-base sm:font-medium text-gray-600">From: {wave.address}</p>
+                  </div>
+                </div>
+                <div className="mt-4 mb-2 sm:mb-0 sm:pr-8">
+                  <p className="text-xs sm:text-sm text-gray-500">{wave.timestamp.toString()}</p>
                 </div>
               </div>
-
-              <div className="mt-4 sm:pr-8">
-                <p className="text-xs sm:text-sm text-gray-500">{wave.timestamp.toString()}</p>
-              </div>
-            </div>
+            </SwiperSlide>
           );
         })}
-      </div>
+      </Swiper>
     </div>
   );
 };
